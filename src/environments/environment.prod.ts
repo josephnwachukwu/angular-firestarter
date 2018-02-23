0ec8d70f574d98a2978e531dbdd9842ebe1d1ed4
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2018-02-23 13:15:28 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,12 @@
 
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyA0mh8xWZUbRkSjZnTS4mmWifl2WAYxZ3A",
+        authDomain: "matchstick-46c7a.firebaseapp.com",
+        databaseURL: "https://matchstick-46c7a.firebaseio.com",
+        projectId: "matchstick-46c7a",
+        storageBucket: "matchstick-46c7a.appspot.com",
+        messagingSenderId: "913658066405"
+    }
+};
